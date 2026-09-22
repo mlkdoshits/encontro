38e9454ec094f88cc2a558ce71da188362504a8a
mlkdoshits <pettersonpereira2026@gmail.com>
2026-09-22 09:33:29 -0300

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos (index.html, css, etc)
+// Servir arquivos estáticos da pasta atual
 app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
